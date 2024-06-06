28fb7710a9fe930af1ea6f6125fc4b525d8722aa
rider143k <150319044+rider143k@users.noreply.github.com>
2024-06-06 21:32:47 +0530

    Update info.py
    Yyy

--- info.py
+++ info.py
@@ -127,7 +127,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "https://gooitchi-bot-ec7a883edfd6.herokuapp.com/")
+URL = environ.get("URL", "https://git.heroku.com/gokuz.git")
 
 
 # Rename Info ; If True Then Bot Rename File Else Not
