f0a038543c30a56c7809ea4c9bacf29af5694f01
rider143k <150319044+rider143k@users.noreply.github.com>
2024-06-12 00:08:13 +0530

    Update genlink.py
    Yes

--- plugins/genlink.py
+++ plugins/genlink.py
@@ -1,9 +1,14 @@
-import re, os, json, base64, logging
+import re
 from pyrogram import filters, Client, enums
 from pyrogram.errors.exceptions.bad_request_400 import ChannelInvalid, UsernameInvalid, UsernameNotModified
 from info import ADMINS, LOG_CHANNEL, FILE_STORE_CHANNEL, PUBLIC_FILE_STORE
 from database.ia_filterdb import unpack_new_file_id
 from utils import temp
+import re
+import os
+import json
+import base64
+import logging
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
