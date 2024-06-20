d13e36409d5349f11478a7dad52a1d3513d3514c
rider143k <150319044+rider143k@users.noreply.github.com>
2024-06-20 16:28:15 +0530

    Update info.py
    Yesr

--- info.py
+++ info.py
@@ -50,7 +50,7 @@ FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://gokubot:Moviebot@cluster0.ao2ps86.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://gokubot:Moviebot@cluster0.ao2ps86.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "ankit1")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'AkBotscollection')
 
